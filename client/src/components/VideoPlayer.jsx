5005d790d4e45a71e8f682f43c2f4bde6da43703
BayC <rachada_32@hotmail.com>
2022-09-06 16:08:29 -0400

    Implement VideoPlayer component

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { SocketContext } from '../SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  paper: {
+    padding: '10px',
+    border: 'solid 2px black',
+    margin: '10px',
+  },
+}));
 
 const VideoPlayer = () => {
-  return <div>VideoPlayer</div>;
+  const classes = useStyles();
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
+  return (
+    <Grid container className={classes.gridContainer}>
+      {/* Our own video */}
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant='h5' gutterBottom>
+              {name || 'My Name'}
+            </Typography>
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+
+      {/* User's video */}
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant='h5' gutterBottom>
+              {call.name || "User's Name"}
+            </Typography>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
 };
 
 export default VideoPlayer;
